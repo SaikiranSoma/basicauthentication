e71884c6222e375a0ab4a4582fd5ffb20da8e071
Saikiran Soma <Saikiran.Soma@ust.com>
2024-08-08 12:14:32 +0530

    authorized

--- BasicAuthenticationNew/Controllers/WeatherForecastController.cs
+++ BasicAuthenticationNew/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BasicAuthenticationNew.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
